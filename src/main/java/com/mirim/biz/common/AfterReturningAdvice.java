20482056bd3e75c4ef69e964e9d6e681c7fb020c
vinszip999 <w2011@e-mirim.hs.kr>
2022-04-15 09:24:51 +0900

    220415 수업(JoinPoint - Before 어드바이스, AfterReturning, AfterThrowing, Around)

--- src/main/java/com/mirim/biz/common/AfterReturningAdvice.java
+++ src/main/java/com/mirim/biz/common/AfterReturningAdvice.java
@@ -1,7 +1,19 @@
 package com.mirim.biz.common;
 
+import com.mirim.biz.user.UserVO;
+import org.aspectj.lang.JoinPoint;
+
 public class AfterReturningAdvice {
-    public void afterReturningLog() {
-        System.out.println("[사후처리] 결과값 리턴 후 동작");
+    public void afterReturningLog(JoinPoint jp, Object returnObj) {
+        String method = jp.getSignature().getName();
+
+        if(returnObj instanceof UserVO){ // 왼쪽과 오른쪽 같은 것인지 비교하는 연산자
+            UserVO user = (UserVO) returnObj; // 다운 캐스팅
+            if(user.getRole().equals("Admin")){ // 관리자인지 확인
+                System.out.println(user.getName() + "로그인(Admin)");
+
+            }
+        }
+        System.out.println("[사후처리]" + method + "() 메소드 리턴 값 : " + returnObj.toString());
     }
 }
